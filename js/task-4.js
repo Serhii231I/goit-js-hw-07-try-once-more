6fbc705bb4f8942fac7e2fccae0bab4f9741eef8
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2026-01-04 19:41:33 +0100

    add all

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (
+    event.target.elements.email.value === "" ||
+    event.target.elements.password.value === ""
+  ) {
+    return alert("All form fields must be filled in");
+  }
+  const info = {
+    login: event.target.email.value.trim(),
+    password: event.target.password.value.trim(),
+  };
+
+  console.log(info);
+  event.target.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyColor = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  console.log(event);
+
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = bodyColor.style.backgroundColor;
+}
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// const body = document.querySelector("body");
+// const button = document.querySelector(".change-color");
+// const span = document.querySelector(".color");
+// button.addEventListener("click", handleClick);
+// function handleClick(event) {
+//   event.preventDefault();
+//   body.style.backgroundColor = getRandomHexColor();
+//   span.textContent = body.style.backgroundColor;
+// }
